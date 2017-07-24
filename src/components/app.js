86999d0bb54197d6624de9288c77d813d07e80d1
Emmanuel Jucaban <EmmanuelJucaban@gmail.com>
2017-07-24 13:55:26 -0700

    Added searchbar container

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 
-const App = () => {
-  return <h1>Hi!</h1>;
-};
+import SearchBar from 'containers/search_bar';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar/>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('styles/vendor');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
