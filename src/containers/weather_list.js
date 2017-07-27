4a4799627a0080c5dfdb75d5a8f8d9e92af21425
Emmanuel Jucaban <EmmanuelJucaban@gmail.com>
2017-07-26 19:24:08 -0700

    Moved charts to a separate component

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,32 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from 'components/chart';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map( weather => weather.main.temp);
-    const pressure = cityData.list.map( weather => weather.main.pressure);
-    const humidity = cityData.list.map( weather => weather.main.humidity);
+    const pressures = cityData.list.map( weather => weather.main.pressure);
+    const humidities = cityData.list.map( weather => weather.main.humidity);
 
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td>
-          <Sparklines height={123} width={180} data={temps}>
-            <SparklinesLine color="red"></SparklinesLine>
-          </Sparklines>
-        </td>
-        <td>
-          <Sparklines height={123} width={180} data={pressure}>
-            <SparklinesLine color="purple"></SparklinesLine>
-          </Sparklines>
-        </td>
-        <td>
-          <Sparklines height={123} width={180} data={humidity}>
-            <SparklinesLine color="blue"></SparklinesLine>
-          </Sparklines>
-        </td>
+        <td><Chart data={temps} color="red"/></td>
+        <td><Chart data={pressures} color="purple"/></td>
+        <td><Chart data={humidities} color="blue"/></td>
       </tr>
     );
   }
